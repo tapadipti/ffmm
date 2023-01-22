1954748c5cbf047e9293ade9dccd48799d13139e
tapadipti <tapadipti@gmail.com>
2023-01-22 15:14:31 +0545

    Build CNN

--- src/train.py
+++ src/train.py
@@ -6,8 +6,6 @@ from tensorflow.keras.utils import to_categorical
 
 from dvclive import Live
 
-from mlem.api import save
-
 OUTPUT_DIR = "output"
 fpath = os.path.join(OUTPUT_DIR, "data.pkl")
 with open(fpath, "rb") as fd:
@@ -31,11 +29,15 @@ hidden_units = params["hidden_units"]
 dropout = params["dropout"]
 num_epochs = params["num_epochs"]
 lr = params["lr"]
+conv_activation = params["conv_activation"]
 
 # Model specific code
-x_train = np.reshape(x_train, [-1, input_size])
+x_train = x_train.reshape(-1, 28, 28, 1)
 x_train = x_train.astype('float32') / 255
 model = Sequential()
+model.add(Conv2D(filters=28, kernel_size=(3,3), activation=conv_activation))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Flatten())
 model.add(Dense(hidden_units, activation='relu'))
 model.add(Dropout(dropout))
 model.add(Dense(num_labels, activation='softmax'))
@@ -80,8 +82,6 @@ with open("output/train_logs.csv", "w") as f:
     f.write(history_to_csv(history))
 
 model.summary()
-
-save(model, "models/fashion_mnist_classifier")
 
 model_file = os.path.join(OUTPUT_DIR, "model.h5")
 model.save(model_file)
